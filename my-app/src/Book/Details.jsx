d298493c0a7b07a5dc5771ce4dc4be4f3e8a9698
hyeeeoon <423nagi@gmail.com>
2025-05-30 15:07:21 +0900

    ..

---
+++ my-app/src/Book/Details.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // 추가
+
+function Details() {
+
+  return (
+    <div>
+        수정
+    </div>
+  );
+}
+
+export default Details;
---
+++ my-app/src/Book/Register.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // 추가
+
+function Register() {
+
+  return (
+    <div>
+        등록
+    </div>
+  );
+}
+
+export default Register;
