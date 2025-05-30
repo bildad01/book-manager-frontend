448811d94a01c929d0f7b7663a4b2592a49fbf8b
sjlee-catthe <ee395000@gmail.com>
2025-05-30 15:14:51 +0900

    심란함

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,14 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0)
+import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import BookList       from './Book/List'
+import BookRegister   from './Book/Register'
+import BookUpdate     from './Book/Update'
+import BookDetails   from './Book/Details'
 
+export default function App() {
   return (
-    <>
-      <Button variant="contained">버튼입니다</Button>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        {/* 1. 도서 목록 (메인) */}
+      
+        <Route path="/" element={<BookList />} />
 
-export default App
+        {/* 2. 도서 등록 */}
+        <Route path="/books/register" element={<BookRegister />} />
+
+        {/* 3. 도서 수정 (URL 파라미터로 id 전달) */}
+        <Route path="/books/update/:id" element={<BookUpdate />} />
+
+        {/* 4. 도서 상세 & 표지 히스토리 (id 전달) */}
+        <Route path="/books/details/:id" element={<BookDetails />} />
+
+        {/* 정의되지 않은 모든 경로는 메인으로 리다이렉트 */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
