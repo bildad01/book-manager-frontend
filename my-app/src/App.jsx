6d23cdf925b378f402bc11a09ba6ed6aa76bc5e3
jeonghani <81557284+jeonghani@users.noreply.github.com>
2025-05-30 16:21:01 +0900

    develop app 수정

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import BookList from './Book/BookList';
-import Details from './Book/Details';
 import Register from './Book/Register';
 // 필요하면 import './App.css'; 도 추가
 
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Navigate to="/book" />} />
         <Route path="/book" element={<BookList />} />
         <Route path="/book/list" element={<BookList />} />
-        <Route path="/book/details" element={<Details />} />
         <Route path="/book/register" element={<Register />} />
       </Routes>
     </Router>
