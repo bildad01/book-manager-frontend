b0c291565c4c4c436849d14693c55c67b5ab87f1
hyeeeoon <423nagi@gmail.com>
2025-05-30 15:20:04 +0900

    css...

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,9 @@
 //import './App.css'
 import { Navigate } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BookList from './BookList';
+import BookList from './Book/BookList';
+import Details from './Book/Details';
+import Register from './Book/Register';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Navigate to="/book" />} />
         <Route path="/book" element={<BookList />} />
         <Route path="/book/list" element={<BookList />} />
+        <Route path="/book/details" element={<Details />} />
+        <Route path="/book/register" element={<Register />} />
       </Routes>
     </Router>
   );
